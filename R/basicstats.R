1af1422572e4aefd13a1dbd83772ef0d7953c1db
kahaaga <kahaaga@gmail.com>
2017-06-16 15:27:21 +0200

    Updated quantiles.

--- R/basicstats.R
+++ R/basicstats.R
@@ -4,17 +4,17 @@
 #' @return A named vector of statistical parameters of the vector 'v'
 #' @export basicstats
 basicstats <- function(v) {
-
-    v.min = base::min(v, na.rm = T)
-    v.max = base::max(v, na.rm = T)
-    v.mean = base::mean(v, na.rm = T)
+    if (class(v) == "data.frame") v = as.ve
+    v.min = min(v, na.rm = T)
+    v.max = max(v, na.rm = T)
+    v.mean = mean(v, na.rm = T)
     v.median = stats::median(v, na.rm = T)
     v.mad = stats::mad(v, na.rm = T)
     v.IQR = stats::IQR(v, na.rm = T)
-    v.p01 = stats::quantile(v, 0.01, na.rm = T)
-    v.p05 = stats::quantile(v, 0.05, na.rm = T)
-    v.p95 = stats::quantile(v, 0.95, na.rm = T)
-    v.p99 = stats::quantile(v, 0.99, na.rm = T)
+    v.p01 = stats::quantile(v, 0.01, na.rm = T)[[1]]
+    v.p05 = stats::quantile(v, 0.05, na.rm = T)[[1]]
+    v.p95 = stats::quantile(v, 0.95, na.rm = T)[[1]]
+    v.p99 = stats::quantile(v, 0.99, na.rm = T)[[1]]
     v.samplesize.with.nans = length(v[complete.cases(v)])
     v.samplesize = length(v)
 
