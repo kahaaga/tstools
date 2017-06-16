263382fedaf7d21e17f3d3feb2c6e69ee2889371
kahaaga <kahaaga@gmail.com>
2017-06-16 15:19:37 +0200

    Handle NANs in basicstats()

--- R/basicstats.R
+++ R/basicstats.R
@@ -5,17 +5,19 @@
 #' @export basicstats
 basicstats <- function(v) {
 
-    v.min = base::min(v)
-    v.max = base::max(v)
-    v.mean = base::mean(v)
-    v.median = stats::median(v)
-    v.mad = stats::mad(v)
-    v.IQR = stats::IQR(v)
-    v.p01 = stats::quantile(v, 0.01)
-    v.p05 = stats::quantile(v, 0.05)
-    v.p95 = stats::quantile(v, 0.95)
-    v.p99 = stats::quantile(v, 0.99)
+    v.min = base::min(v, na.rm = T)
+    v.max = base::max(v, na.rm = T)
+    v.mean = base::mean(v, na.rm = T)
+    v.median = stats::median(v, na.rm = T)
+    v.mad = stats::mad(v, na.rm = T)
+    v.IQR = stats::IQR(v, na.rm = T)
+    v.p01 = stats::quantile(v, 0.01, na.rm = T)
+    v.p05 = stats::quantile(v, 0.05, na.rm = T)
+    v.p95 = stats::quantile(v, 0.95, na.rm = T)
+    v.p99 = stats::quantile(v, 0.99, na.rm = T)
+    v.samplesize.with.nans = length(v[complete.cases(v)])
     v.samplesize = length(v)
+
     v.outliers.fraction = length(grDevices::boxplot.stats(v))/v.samplesize
 
 
@@ -30,5 +32,6 @@ basicstats <- function(v) {
              "p95" = v.p95,
              "p99" = v.p99,
              "samplesize" = v.samplesize,
+             "samplesize.withnans" = v.samplesize.with.nans,
              "outlier.fraction" = v.outliers.fraction))
 }
