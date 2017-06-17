812567d0fdae93ea1d61f7c176af4f2b8a12db53
kahaaga <kahaaga@gmail.com>
2017-06-17 21:11:54 +0200

    Updated variable names according to common naming convention.

--- R/remove_outliers.R
+++ R/remove_outliers.R
@@ -2,7 +2,7 @@
 #'
 #' @param v A scalar valued vector.
 #' @return The vector 'v' with the outliers removed.
-#' @export remove.outliers
-remove.outliers <- function(v) {
+#' @export remove_outliers
+remove_outliers <- function(v) {
     v[!v %in% grDevices::boxplot.stats(v)$out]
 }
