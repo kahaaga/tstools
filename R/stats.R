bfe984a9e01a3e56cbe52dc034bd31e4eae30872
kahaaga <kahaaga@gmail.com>
2017-06-17 22:50:20 +0200

    Added additional statistical that are usefor for boxplots.

--- R/stats.R
+++ R/stats.R
@@ -4,19 +4,25 @@
 #' @return A named vector of statistical parameters of the vector 'v'
 #' @export stats
 stats <- function(v) {
-    if (class(v) == "data.frame") v = as.ve
+    if (class(v) == "data.frame") v = as.vector(v)
+
+    v.samplesize.without.nans = length(v[complete.cases(v)])
+    v.samplesize = length(v)
+
     v.min = min(v, na.rm = T)
     v.max = max(v, na.rm = T)
     v.mean = mean(v, na.rm = T)
     v.median = stats::median(v, na.rm = T)
     v.mad = stats::mad(v, na.rm = T)
     v.IQR = stats::IQR(v, na.rm = T)
+    Q1 = quantile(v, 0.25, na.rm = T)
+    Q3 = quantile(v, 0.75, na.rm = T)
+    notchlower = v.median - (1.58 * v.IQR/sqrt(v.samplesize.without.nans))
+    notchupper = v.median + (1.58 * v.IQR/sqrt(v.samplesize.without.nans))
     v.p01 = stats::quantile(v, 0.01, na.rm = T)[[1]]
     v.p05 = stats::quantile(v, 0.05, na.rm = T)[[1]]
     v.p95 = stats::quantile(v, 0.95, na.rm = T)[[1]]
     v.p99 = stats::quantile(v, 0.99, na.rm = T)[[1]]
-    v.samplesize.with.nans = length(v[complete.cases(v)])
-    v.samplesize = length(v)
 
     v.outliers.fraction = length(grDevices::boxplot.stats(v))/v.samplesize
 
@@ -27,11 +33,15 @@ stats <- function(v) {
              "median" = v.median,
              "MAD" = v.mad,
              "IQR" = v.IQR,
-             "p01" = v.p01,
-             "p05" = v.p05,
-             "p95" = v.p95,
-             "p99" = v.p99,
+             "Q1"  = Q1,
+             "Q3"  = Q3,
+             "notchlower" = notchlower,
+             "notchupper" = notchupper,
+             "1st.percentile" = v.p01,
+             "5th.percentile" = v.p05,
+             "95th.percentile" = v.p95,
+             "99th.percentile" = v.p99,
              "samplesize" = v.samplesize,
-             "samplesize.withnans" = v.samplesize.with.nans,
+             "samplesize.without.nans" = v.samplesize.without.nans,
              "outlier.fraction" = v.outliers.fraction))
 }
