c982c0546fa7487586ed6c752b11b91deead77f4
kahaaga <kahaaga@gmail.com>
2017-06-24 20:27:44 +0200

    Documentation and minor fixes.

--- R/draw-random-series.R
+++ R/draw-random-series.R
@@ -1,6 +1,6 @@
 #' Draws random datagiven a vector 'data' and
 #' its associated uncertainties, either in the form of
-#' 'sigmas' or a set of 'lower_bounds' and 'upper_bounds'.
+#' 'sigmas' or a set of 'lower.bounds' and 'upper.bounds'.
 #' If 'sigmas' is provided, draw from a Gaussian centered
 #' on the data. If bounds are provided, draw from a
 #' Gaussian truncated at the lower and upper bounds.
@@ -8,31 +8,31 @@
 #' @param data A vector of data.
 #' @param sigmas A vector of 1 sigma uncertainties associated with the data.
 #' @param n.sigma How many sigmas should be allowed?
-#' @param lower_bounds A vector of lower bounds for the data.
-#' @param upper_bounds A vector of upper bounds for the data.
+#' @param lower.bounds A vector of lower bounds for the data.
+#' @param upper.bounds A vector of upper bounds for the data.
 #' @return  A matrix of randomly drawn data.
 #'
 #' @export draw_random_data
 draw_random_data <- function(data,
-                       sigmas = NA,
+                       sigmas = NULL,
                        n.sigma = 2,
                        n.replicates = 1,
-                       lower_bounds = NA,
-                       upper_bounds = NA) {
+                       lower.bounds = NULL,
+                       upper.bounds = NULL) {
 
-    models = replicate(n = n.models,
+    models = replicate(n = n.replicates,
                        expr = datamodel(data = data,
                                         sigmas = sigmas,
                                         n.sigma = n.sigma,
-                                        lower_bounds = lower_bounds,
-                                        upper_bounds = upper_bounds),
+                                        lower.bounds = lower.bounds,
+                                        upper.bounds = upper.bounds),
                        simplify = TRUE)
     return(models)
 }
 
 #' Draws a random data model given a vector 'data' and
 #' its associated uncertainties, either in the form of
-#' 'sigmas' or a set of 'lower_bounds' and 'upper_bounds'.
+#' 'sigmas' or a set of 'lower.bounds' and 'upper.bounds'.
 #' If 'sigmas' is provided, draw from a Gaussian centered
 #' on the data. If bounds are provided, draw from a
 #' Gaussian truncated at the lower and upper bounds.
@@ -44,23 +44,32 @@ draw_random_data <- function(data,
 #' @param upper.bounds A vector of upper bounds for the data.
 #' @return A vector containing the
 datamodel <- function(data,
-                      sigmas = NA,
+                      sigmas = NULL,
                       n.sigma = 2,
-                      lower.bounds = NA,
-                      upper.bounds = NA) {
+                      lower.bounds = NULL,
+                      upper.bounds = NULL) {
     if (any(is.na(sigmas)) == FALSE) {
-        return(datamodel_sigmas(data = data,
-                                sigmas = sigmas,
-                                n.sigma = n.sigma))
-    } else if (is.vector(lower.bounds) && is.vector(upper.bounds)) {
-        return(datamodel_truncated(data = data,
-                                sigmas = sigmas,
-                                n.sigma = n.sigma))
+      return(datamodel_sigmas(data = data,
+                              sigmas = sigmas,
+                              n.sigma = n.sigma))
+
+    } else if (is.null(sigmas) ||
+              (is.vector(lower.bounds) && is.vector(upper.bounds))) {
+      return(datamodel_truncated(data = data,
+                                 lower.bounds = lower.bounds,
+                                 upper.bounds = upper.bounds,
+                                 n.sigma = n.sigma))
     } else {
         warning("Input to datamodel() is not valid.")
     }
 }
 
+#' Draws random data according to a Gaussian distribution centered
+#' around the original data with standard deviations given by
+#' n.sigma * sigmas.
+#' @param data A vector of data.
+#' @param sigmas A vector of standard deviations.
+#' @param n.sigma The number of standard deviations for the Gaussian.
 datamodel_sigmas <- function(data, sigmas, n.sigma) {
     datamodel = matrix(nrow = length(data), ncol = 1)
     for (i in 1:length(data)) {
@@ -71,6 +80,13 @@ datamodel_sigmas <- function(data, sigmas, n.sigma) {
     return(datamodel)
 }
 
+#' Draws random data according to a Gaussian with standard deviation
+#' n.sigma centered at the original data and truncated at the
+#' lower.bounds and upper.bounds.
+#' @param data A vector of data.
+#' @param lower.bounds A vector of lower bounds.
+#' @param upper.bounds A vector of upper bounds.
+#' @param n.sigma The number of standard deviations for the Gaussian.
 datamodel_truncated <- function(data, lower.bounds, upper.bounds, n.sigma) {
     datamodel = matrix(nrow = length(data), ncol = 1)
     for (i in 1:length(data)) {
