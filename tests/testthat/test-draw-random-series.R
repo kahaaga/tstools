abe746e5dd0cd98b970a5add1c469e342f5f4d6c
kahaaga <kahaaga@gmail.com>
2017-06-25 04:36:12 +0200

    Added more tests for random series draw.

--- tests/testthat/test-draw-random-series.R
+++ tests/testthat/test-draw-random-series.R
@@ -6,14 +6,34 @@ sigmas = rnorm(n = n, sd = sd(dt) * 0.1)
 lower.bounds = -abs(rnorm(n = n, sd = sd(dt) * 0.1))
 upper.bounds = abs(rnorm(n = n, sd = sd(dt) * 0.1))
 
+
 test_that("Single data model based on sigmas works", {
-  expect_that(length(datamodel(data = dt, sigmas = sigmas)),
+  expect_that(length(datamodel_sigmas(data = dt, sigmas = sigmas, n.sigma = 1)),
                      equals(n))
 })
 
 test_that("Single data model based on upper and lower bounds works", {
-  expect_that(length(datamodel(data = dt,
+  expect_that(length(datamodel_truncated(data = dt,
                                upper.bounds = upper.bounds,
-                               lower.bounds = lower.bounds)),
+                               lower.bounds = lower.bounds,
+                               n.sigma = 1)),
               equals(n))
 })
+
+test_that("General data model succeeds irrespective of type of standard deviations", {
+
+  expect_that(length(datamodel(data = dt, sigmas = sigmas, n.sigma = 2)), equals(n))
+  expect_that(length(datamodel(data = dt,
+                               upper.bounds = upper.bounds,
+                               lower.bounds = lower.bounds,
+                               n.sigma = 2)),
+              equals(n))
+
+  expect_that(ncol(draw_random_data(dt, sigmas = sigmas, n.replicates = 5)), equals(5))
+  expect_that(ncol(draw_random_data(dt,
+                                    lower.bounds = lower.bounds,
+                                    upper.bounds = upper.bounds,
+                                    n.replicates = 5)), equals(5))
+
+})
+
