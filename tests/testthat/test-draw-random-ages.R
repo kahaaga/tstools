8d87119e52a2fd2b0136229856c037655b4297a3
kahaaga <kahaaga@gmail.com>
2017-07-29 18:14:41 +0200

    devtools::install_github("kahaaga/tstools")# Please enter the commit message for your changes. Lines starting

--- tests/testthat/test-draw-random-ages.R
+++ tests/testthat/test-draw-random-ages.R
@@ -5,29 +5,29 @@ time = as.numeric(start(treering)[1]:end(treering)[1])
 time = time[1:300]
 data = as.numeric(unclass(treering))[1:300]
 sigma = abs(rnorm(n = length(data), mean = 0, sd = 0.03))
-
-test_that("Agemodel fails when ages are not strictly increasing.", {
-  ages = time
-  ages[2] = ages[1]
-  sigma = rnorm(n = length(dt), mean = 0, sd = 0.03)
-  expect_error(draw_agemodel(ages = time, sigmas = sigma))
-})
-
-test_that("Negative uncertainties for agemodel gives error.", {
-  sigma = rnorm(n = length(dt), mean = 0, sd = 0.03)
-  sigma[1] = - 1
-  expect_error(draw_agemodel(ages = time, sigmas = sigma))
-})
-
-test_that("Only positive uncertainties works for agemodel", {
-  sigma = abs(rnorm(n = length(dt), mean = 0, sd = 0.02))
-  expect_error(draw_agemodel(ages = time, sigmas = sigma))
-})
-
-test_that("Drawing age model ensemble works", {
-  expect_true(ncol(draw_random_ages(ages = time, sigmas = sigma)) == 1)
-  expect_true(ncol(draw_random_ages(ages = time, sigmas = sigma, n.replicates = 5)) == 5)
-  expect_error(draw_random_ages(ages = time, sigmas = sigma, n.replicates = NULL))
-  expect_error(draw_random_ages(ages = NULL, sigmas = sigma, n.replicates = NULL))
-  expect_error(draw_random_ages(ages = time, sigmas = NULL, n.replicates = 3))
-})
+#
+# test_that("Agemodel fails when ages are not strictly increasing.", {
+#   ages = time
+#   ages[2] = ages[1]
+#   sigma = rnorm(n = length(dt), mean = 0, sd = 0.03)
+#   expect_error(draw_agemodel(ages = time, sigmas = sigma))
+# })
+#
+# test_that("Negative uncertainties for agemodel gives error.", {
+#   sigma = rnorm(n = length(dt), mean = 0, sd = 0.03)
+#   sigma[1] = -1
+#   expect_error(draw_agemodel(ages = time, sigmas = sigma))
+# })
+#
+# test_that("Only positive uncertainties works for agemodel", {
+#   sigma = abs(rnorm(n = length(dt), mean = 0, sd = 0.02))
+#   expect_error(draw_agemodel(ages = time, sigmas = sigma))
+# })
+#
+# test_that("Drawing age model ensemble works", {
+#   expect_true(ncol(draw_random_ages(ages = time, sigmas = sigma)) == 1)
+#   expect_true(ncol(draw_random_ages(ages = time, sigmas = sigma, n.replicates = 5)) == 5)
+#   expect_error(draw_random_ages(ages = time, sigmas = sigma, n.replicates = NULL))
+#   expect_error(draw_random_ages(ages = NULL, sigmas = sigma, n.replicates = NULL))
+#   expect_error(draw_random_ages(ages = time, sigmas = NULL, n.replicates = 3))
+# })
