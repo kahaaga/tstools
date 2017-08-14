2a880f767f70109cb10dfa45a14b1b42f40bf5ac
kahaaga <kahaaga@gmail.com>
2017-08-14 06:45:30 +0200

    External function calls made explicit.

--- R/surrogate-iterated-aaft.R
+++ R/surrogate-iterated-aaft.R
@@ -36,8 +36,8 @@ iaaft_surrogate <- function(series, n.max.iter = 150) {
 
   # Compute root mean square difference between original and randomly sorted
   # series
-  acf.diff.old = sqrt(sum((acf(series, n - 1, plot = F)$acf -
-                  acf(series.randsorted, n - 1, plot = F)$acf)^2))
+  acf.diff.old = sqrt(sum((stats::acf(series, n - 1, plot = F)$acf -
+                  stats::acf(series.randsorted, n - 1, plot = F)$acf)^2))
 
 
   while (!convergence.achieved && iteration <= n.max.iter) {
@@ -52,7 +52,7 @@ iaaft_surrogate <- function(series, n.max.iter = 150) {
     new_fft = original_fft_amplitudes * exp(randomised.phases * 1i)
 
     # fft with inverse = T returns unnormalised values, so normalise by n
-    surrogate = Re(fft(new_fft, inverse = T))/n # Sample the real part
+    surrogate = Re(stats::fft(new_fft, inverse = T))/n # Sample the real part
 
     surrogate[sort(surrogate, index.return=T)$ix] = sort(series)
 
