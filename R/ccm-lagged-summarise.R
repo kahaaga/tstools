b2c09997cc29c949f17d3f1fbb0be59807264b47
kahaaga <kahaaga@gmail.com>
2017-11-23 09:24:36 +0100

    Rewrote directionalcausaltest function to accept non-convergent analyses

--- R/ccm-lagged-summarise.R
+++ R/ccm-lagged-summarise.R
@@ -1,55 +1,72 @@
-#' @title directionalcausaltest
+#' @name directionalcausaltest
 #' @description Summarise a lagged CCM result.
-#' @details
-#'    CCM predictability for a given lag is only considered if the analysis is
-#'    convergent.
-#'
-#'    Convergence is determined by Wilcoxon rank-sum test on the smallest and largest library sizes. If CCM
+#' @details CCM predictability for a given lag is only considered if the analysis is
+#'    convergent. Convergence is determined by Wilcoxon rank-sum test on the smallest and largest library sizes. If CCM
 #'    skill for the largest library sizes is higher (p < 0.01) than for the
 #'    lowest library sizes, then the analysis is potentially convergent.
 #'    Additionally, the median CCM curve over increasing library sizes must be
 #'    satisfactorily fitted by a convergent exponential function. If both these
-#'    criteria are met, then the analysis is labelled convergent.
-#'
-#'    The only grouping variable in this summary function is the lag, so all
+#'    criteria are met, then the analysis is labelled convergent. The only grouping variable in this summary function is the lag, so all
 #'    further groupings must be handled outside this function.
 #' @param dt A data frame containing the result of a tstools::ccm_lagged() call.
+#' @param library.size The library size to summarise over. Defaults to the
+#'     largest available library size.
 #' @return The difference between summed negative median CCM skills and
 #'     summed positive median CCM skills. If the sum > 0, then the analysis
 #'     passes the lag test.
 #' @examples
 #' # res is the result of a tstools::ccm_lagged() call
-#'library(dplyr)
-#'res %>% directionalcausaltest
+#' library(dplyr)
+#' res %>% directionalcausaltest
 #'
-#'# res is a data frame containing vertically concatenated results of
-#'# tstools::ccm_lagged() calls for different causal variables.
-#'res %>%
-#'  group_by(causal.direction) %>%
-#'  do(directionalcausaltest(.))
-directionalcausaltest <- function(res) {
+#' # res is a data frame containing vertically concatenated results of
+#' # tstools::ccm_lagged() calls for different causal variables.
+#' res %>%
+#'   group_by(causal.direction) %>%
+#'   do(directionalcausaltest(.))
+#' @export
+directionalcausaltest <- function(res, library.size = max(res$library.size)) {
+  res = res %>% filter(library.size == library.size)
+  results = list()
 
-  dt = res %>%
-    dplyr::group_by(lag) %>%
-    dplyr::filter(convergent == 1, p.value < 0.01, k > 0) %>%
-    dplyr::do(as.data.frame(as.list(summary_stats(.$rho))))
+  # A little trickery to deal with analyses that are not convergent
+  # for a specific lag. Returns a row of NA/Inf statistics, but with
+  # the same column names as usual if that is the case.
+  for (lag in unique(res$lag)) {
+    results_this_lag = res %>%
+      filter(lag == lag) %>%
+      filter(convergent == 1, p.value < 0.01, k > 0)
 
-  negatives = dt %>% filter(lag < 0)
-  positives = dt %>% filter(lag > 0)
+    # If there are no convergent analyses, run dummy summary statistics on
+    # NA that yields a row of NAs of Inf/-Infs for the statistics.
+    if (nrow(results_this_lag) == 0) {
+      statsummary = tstools::summary_stats(c(NA), as.df = T)
+    } else {# Do regular stats.
+      statsummary = tstools::summary_stats(results_this_lag$rho, as.df = T)
+    }
+    # Add information about lag again.
+    results[[toString(l)]] = cbind(data.frame(lag = l), statsummary)
+  }
+  # Combine results for all the lags.
+  res = results %>% bind_rows
 
-  # If the following sum > 0, then the analysis is causal.
-  if (nrow(negatives) == 0 & nrow(positives) == 0) {
+  # Separate negative and positive lags.
+  negatives = res %>% filter(lag < 0) %>% select(median) %>% sum
+  positives = res %>% filter(lag > 0) %>% select(median) %>% sum
+
+  # Take the difference between causal (negative lags) and non-causal (positive
+  # lags) skills.
+  if (is.na(negatives) & is.na(positives)) {
     causaldifference = NA
-  } else if (nrow(negatives) > 0 & nrow(positives) > 0) {
-    causaldifference = sum(negatives$median) - sum(positives$median)
-  } else if (nrow(negatives) > 0 & nrow(positives) == 0) {
-    causaldifference = sum(negatives$median)
-  } else if (nrow(negatives) == 0 & nrow(positives) > 0) {
-    causaldifference = sum(positives$median)
+  } else if (is.finite(negatives) & is.finite(positives)) {
+    causaldifference = negatives - positives
+  } else if (is.na(negatives) & is.finite(positives)) {
+    causaldifference = positives
+  } else if (is.finite(negatives) & is.na(positives)) {
+    causaldifference = negatives
   }
 
   # Add the causal difference sum to the data as repeated values in a column.
-  dt$causaldifference = rep(causaldifference)
-
-  return(dt)
+  res$causaldifference = rep(causaldifference)
+  return(res)
 }
