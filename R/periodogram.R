d3261b83fa4ecd0fc8ca6cbda180db997002f0d2
kahaaga <kahaaga@gmail.com>
2017-11-25 14:38:11 +0100

    Updated docs.

--- R/periodogram.R
+++ R/periodogram.R
@@ -2,8 +2,13 @@
 #' Computes a periodogram for a time series. Wrapper around multitaper::spec.
 #' @param ts A time series of equally spaced data for which to compute the periodogram.
 #' @param nw nw a positive double precision number, the time-bandwidth parameter.
-#' @param dtUnits the units of the time
-#' @param deltat time incrment in units of dtUnits
+#' @param dtUnits The time unit for the time series.
+#' @param deltat The time series is converted to a stats::ts object before
+#'    being fed to multitaper::spec.mtm. deltat gives time increment for ths
+#'    stats::ts object in terms of dtUnits
+#' @param frequency The time series is converted to a stats::ts object before
+#'    being fed to multitaper::spec.mtm. dtUnits gives the frequency for
+#'    the stats::ts object.
 #' @param k k a positive integer, the number of tapers, often 2*nw.
 #' @export
 periodogram <- function(ts,
