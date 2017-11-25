24d47ae44e77db7e3d8c2dd36d746bdaa886bc13
kahaaga <kahaaga@gmail.com>
2017-11-25 13:54:04 +0100

    Updated docs.

--- R/periodogram.R
+++ R/periodogram.R
@@ -2,13 +2,19 @@
 #' Computes a periodogram for a time series. Wrapper around multitaper::spec.
 #' @param ts A time series of equally spaced data for which to compute the periodogram.
 #' @param nw nw a positive double precision number, the time-bandwidth parameter.
+#' @param dtUnits the units of the time
+#' @param deltat time incrment in units of dtUnits
 #' @param k k a positive integer, the number of tapers, often 2*nw.
 #' @export
 periodogram <- function(ts,
-                        frequency = 1, dtUnits = "year", deltat = 1000,
-                        nw = 4.0, k = 2*nw) {
+                        frequency = 1,
+                        dtUnits = "year",
+                        deltat = 1000,
+                        nw = 4.0,
+                        k = 2*nw) {
   timeseries = stats::ts(ts, deltat = 1, frequency = 1)
-  multitaper.spec = multitaper::spec.mtm(timeseries, dtUnits = dtUnits, deltat = deltat, nw = nw, k = k)
+  multitaper.spec = multitaper::spec.mtm(timeseries, dtUnits = dtUnits,
+                                         deltat = deltat, nw = nw, k = k)
   spec = multitaper.spec$spec
   freq = multitaper.spec$freq
 
