1a19ec95b90edb8bf2ca1aea3b6b96a84f59609e
kahaaga <kahaaga@gmail.com>
2017-11-26 20:58:27 +0100

    Added CCM tests with surrogates.

---
+++ tests/testthat/test-ccm-surrogate.R
@@ -0,0 +1,13 @@
+context("Convergent cross mapping (CCM)")
+
+
+# Default for exclusion radius, embedding lag and embedding dimension
+suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80),
+             lags = 0,
+             convergence.test = T,
+             samples.original = 25,
+             samples.surrogates = 25,
+             n.surrogates = 25,
+             taus = 1,  Es = 2)
+)
