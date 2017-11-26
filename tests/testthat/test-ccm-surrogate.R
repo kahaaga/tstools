5a3fb93d2f4d967a600390678568914cd20cb36b
kahaaga <kahaaga@gmail.com>
2017-11-26 23:54:31 +0100

    Surrogate CCM tests.

--- tests/testthat/test-ccm-surrogate.R
+++ tests/testthat/test-ccm-surrogate.R
@@ -1,14 +1,23 @@
 context("Convergent cross mapping (CCM) with surrogates")
 
+suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80),
+             lags = 0,
+             samples.original = 25,
+             samples.surrogates = 25,
+             surrogate.methods = c("aaft", "random", "iaaft"),
+             n.surrogates = 25,
+             taus = 1,  Es = 2,
+             parallel = F)
+)
 
-# Default for exclusion radius, embedding lag and embedding dimension
-# suppressWarnings(
-#   ccm_lagged(data = chaoticmaps::dejong_map(n = 80),
-#              lags = 0,
-#              convergence.test = T,
-#              samples.original = 25,
-#              samples.surrogates = 25,
-#              surrogate.methods = c("aaft", "random", "iaaft"),
-#              n.surrogates = 25,
-#              taus = 1,  Es = 2)
-# )
+suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80),
+             lags = 0,
+             samples.original = 25,
+             samples.surrogates = 25,
+             surrogate.methods = c("aaft", "random", "iaaft"),
+             n.surrogates = 25,
+             taus = 1,  Es = 2,
+             parallel = T)
+)
