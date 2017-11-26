21e27df807ebba263d88ba73b45aeebbfe395d57
kahaaga <kahaaga@gmail.com>
2017-11-26 18:40:12 +0100

    CCM tests.

--- tests/testthat/test-ccm.R
+++ tests/testthat/test-ccm.R
@@ -2,19 +2,61 @@ context("Convergent cross mapping (CCM)")
 
 dt = rbind(CO2[4:5], CO2[4:5])[1:100, ]
 
-# Explicitly set embedding dimension
-ccm = suppressWarnings(
-  ccm_lagged(data = dt, lags = 0, E = 2, tau = 1,
-             samples.original = 30,
-             n.surrogates = 0)
-)
-# Optimise embedding dimension
-# ccm = suppressWarnings(
-#   ccm_lagged(data = dt, lags = -1:1,
-#            samples.original = 30,
-#            samples.surrogates = 30,
-#            n.surrogates = 20)
-#   )
-#
 
-#summary = directionalcausaltest(res = ccm)
+# Default for exclusion radius, embedding lag and embedding dimension
+ex1a = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+             exclusion.radius = NULL,
+             taus = NULL,  Es = NULL)
+)
+
+
+# Default for exclusion radius, embedding lag and embedding dimension
+ex1b = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+             exclusion.radius = "mi",
+             taus = NULL,  Es = NULL)
+)
+
+
+# Default for exclusion radius, embedding lag and embedding dimension
+ex1b = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+             exclusion.radius = "acf",
+             taus = NULL,  Es = NULL)
+)
+
+# Explicitly set embedding lag and embedding dimensions, default for exclusion radius.
+ex2 = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+            exclusion.radius = NULL,
+            taus = c(1, 2), Es = c(2, 3))
+  )
+
+# Default for exclusion radius, embedding lag and embedding dimension
+ex3 = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+             exclusion.radius = NULL,
+             taus = NULL,  Es = NULL)
+)
+
+# Optimise tau
+ex3 = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+             exclusion.radius = NULL,
+             taus = "mi",  Es = NULL)
+)
+
+# Optimise tau
+ex3 = suppressWarnings(
+  ccm_lagged(data = chaoticmaps::dejong_map(n = 80), lags = 0,
+             samples.original = 30,
+             exclusion.radius = NULL,
+             taus = "acf",  Es = NULL)
+)
