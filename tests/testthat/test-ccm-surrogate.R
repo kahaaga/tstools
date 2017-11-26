efc564203c2b562d27f187e9325866a3f45452f8
kahaaga <kahaaga@gmail.com>
2017-11-26 21:21:43 +0100

    Travis fails. Check if surrogate CCM test is the culprit.

--- tests/testthat/test-ccm-surrogate.R
+++ tests/testthat/test-ccm-surrogate.R
@@ -2,12 +2,13 @@ context("Convergent cross mapping (CCM) with surrogates")
 
 
 # Default for exclusion radius, embedding lag and embedding dimension
-suppressWarnings(
-  ccm_lagged(data = chaoticmaps::dejong_map(n = 80),
-             lags = 0,
-             convergence.test = T,
-             samples.original = 25,
-             samples.surrogates = 25,
-             n.surrogates = 25,
-             taus = 1,  Es = 2)
-)
+# suppressWarnings(
+#   ccm_lagged(data = chaoticmaps::dejong_map(n = 80),
+#              lags = 0,
+#              convergence.test = T,
+#              samples.original = 25,
+#              samples.surrogates = 25,
+#              surrogate.methods = c("aaft", "random", "iaaft"),
+#              n.surrogates = 25,
+#              taus = 1,  Es = 2)
+# )
