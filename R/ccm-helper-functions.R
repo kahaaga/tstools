94180944fc333989ac2319d4330ba4dfbce7a9cd
kahaaga <kahaaga@gmail.com>
2017-11-26 17:11:18 +0100

    Conform to style standard.

--- R/ccm-helper-functions.R
+++ R/ccm-helper-functions.R
@@ -8,21 +8,21 @@ column_names_as_string <- function(column.names,
       is.numeric(target.column) &
       is.numeric(surrogate.column)) {
 
-    library.column = column.names[library.column]
-    target.column = column.names[target.column]
-    surrogate.column =column.names[surrogate.column]
+    library.column <- column.names[library.column]
+    target.column <- column.names[target.column]
+    surrogate.column <- column.names[surrogate.column]
 
   } else {
-    library.column = ifelse(test = is.numeric(library.column),
+    library.column <- ifelse(test = is.numeric(library.column),
                             yes = column.names[library.column],
                             no = library.column)
 
-    target.column = ifelse(test = is.numeric(target.column),
-                           yes =column.names[target.column],
+    target.column <- ifelse(test = is.numeric(target.column),
+                           yes = column.names[target.column],
                            no = target.column)
 
-    surrogate.column = ifelse(test = is.numeric(surrogate.column),
-                              yes =column.names[surrogate.column],
+    surrogate.column <- ifelse(test = is.numeric(surrogate.column),
+                              yes = column.names[surrogate.column],
                               no = surrogate.column)
   }
 
