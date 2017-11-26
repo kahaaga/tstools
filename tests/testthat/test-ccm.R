7d46bea8bcea811c09149293301af6fc850172e3
kahaaga <kahaaga@gmail.com>
2017-11-26 15:22:14 +0100

    Try to run basic CCM test.

--- tests/testthat/test-ccm.R
+++ tests/testthat/test-ccm.R
@@ -2,6 +2,12 @@ context("Convergent cross mapping (CCM)")
 
 dt = rbind(CO2[4:5], CO2[4:5])[1:100, ]
 
+# Explicitly set embedding dimension
+ccm = suppressWarnings(
+  ccm_lagged(data = dt, lags = 0, E = 2, tau = 1,
+             samples.original = 30,
+             n.surrogates = 0)
+)
 # Optimise embedding dimension
 # ccm = suppressWarnings(
 #   ccm_lagged(data = dt, lags = -1:1,
@@ -10,10 +16,5 @@ dt = rbind(CO2[4:5], CO2[4:5])[1:100, ]
 #            n.surrogates = 20)
 #   )
 #
-# # Explicitly set embedding dimension
-# ccm = suppressWarnings(
-#   ccm_lagged(data = dt, lags = 0, E = 2, tau = 1,
-#              samples.original = 30,
-#              n.surrogates = 0)
-# )
+
 #summary = directionalcausaltest(res = ccm)
